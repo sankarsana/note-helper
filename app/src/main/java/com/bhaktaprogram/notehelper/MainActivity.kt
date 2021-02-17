2fc634bb324d435f9105ab17f896a322e4633708
Sankarsana <sankarsana.nrs@gmail.com>
2021-02-17 21:00:53 +0300

    Add empty notes fragment and viewModel

--- app/src/main/java/com/bhaktaprogram/notehelper/MainActivity.kt
+++ app/src/main/java/com/bhaktaprogram/notehelper/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.bhaktaprogram.notehelper
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.bhaktaprogram.notehelper.di.DaggerNotesComponent
+import com.bhaktaprogram.notehelper.features.notes.NotesFragment
 import com.bhaktaprogram.notehelper.features.notes.NotesRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.Default
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -21,12 +18,12 @@ class MainActivity : AppCompatActivity() {
 
         DaggerNotesComponent.create().inject(this)
 
-        CoroutineScope(Default).launch {
-            val notes = repository.getAll()
-
-            if (notes.isEmpty()) throw RuntimeException()
-
-            Log.i("TAG", notes.toString())
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragments_container, NotesFragment())
+                .commit()
         }
+
     }
 }
---
+++ app/src/main/java/com/bhaktaprogram/notehelper/features/notes/NotesViewModel.kt
@@ -0,0 +1,7 @@
+package com.bhaktaprogram.notehelper.features.notes
+
+import androidx.lifecycle.ViewModel
+
+class NotesViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
