8ee6d523e9743cbb521f324fa8024b2705593f77
Sankarsana <sankarsana.nrs@gmail.com>
2021-02-18 19:14:19 +0300

    Impl viewBinding in NotesFragment

--- app/src/main/java/com/bhaktaprogram/notehelper/features/notes/ui/NotesFragment.kt
+++ app/src/main/java/com/bhaktaprogram/notehelper/features/notes/ui/NotesFragment.kt
@@ -1,10 +1,11 @@
 package com.bhaktaprogram.notehelper.features.notes.ui
 
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bhaktaprogram.notehelper.R
+import com.bhaktaprogram.notehelper.databinding.NotesFragmentBinding
 import com.bhaktaprogram.notehelper.features.notes.di.DaggerNotesComponent
 import com.bhaktaprogram.notehelper.features.notes.domain.NoteInfo
 import javax.inject.Inject
@@ -14,10 +15,15 @@ class NotesFragment : Fragment(R.layout.notes_fragment) {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
     private lateinit var viewModel: NotesViewModel
+    private var _binding: NotesFragmentBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         DaggerNotesComponent.create().inject(this)
+
+        _binding = NotesFragmentBinding.bind(view)
 
         viewModel = ViewModelProvider(this, viewModelFactory)
             .get(NotesViewModel::class.java)
@@ -26,8 +32,16 @@ class NotesFragment : Fragment(R.layout.notes_fragment) {
     }
 
     private fun updateState(list: List<NoteInfo>) {
+        val notes = StringBuilder("Notes:\n")
         list.forEach {
-            Log.i("TAG", it.toString())
+            notes.append("\t").append(it.toString()).append("\n")
         }
+        notes.trim()
+        binding.textView.text = notes
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
