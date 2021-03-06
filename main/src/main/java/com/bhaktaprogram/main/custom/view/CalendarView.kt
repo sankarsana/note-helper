f881f8b651e63988464c00a58e305cc65987750a
Sankarsana <sankarsana.nrs@gmail.com>
2021-03-06 19:49:30 +0300

    Move day rect to separate class

--- main/src/main/java/com/bhaktaprogram/main/custom/view/CalendarView.kt
+++ main/src/main/java/com/bhaktaprogram/main/custom/view/CalendarView.kt
@@ -2,30 +2,23 @@ package com.bhaktaprogram.main.custom.view
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 import com.bhaktaprogram.main.R
-import kotlin.math.min
 
 class CalendarView @JvmOverloads constructor(
     context: Context,
     attributeSet: AttributeSet? = null
 ) : View(context, attributeSet) {
 
-    private var cellWidth = 0f
-    private var cellHeight = 0f
-    private var horizontalPadding = 0f
-    private var verticalPadding = 0f
-    private var dayViewSize = 0f
-    private val dayRect = RectF()
     private var days = emptyList<DayOfMonthUi>()
     private val paints = Paints(context)
+    private val dayRect = DayRect(
+        resources.getDimensionPixelSize(R.dimen.day_of_month_default_padding).toFloat()
+    )
 
     init {
-        if (isInEditMode) {
-            days = FakeDaysRepository.get()
-        }
+        if (isInEditMode) days = FakeDaysRepository.get()
     }
 
     fun setData(days: List<DayOfMonthUi>) {
@@ -38,34 +31,18 @@ class CalendarView @JvmOverloads constructor(
     }
 
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
-        cellWidth = width.toFloat() / COLUMNS
-        cellHeight = height.toFloat() / ROWS
-        val cellPadding = resources
-            .getDimensionPixelSize(R.dimen.day_of_month_default_padding).toFloat()
-        dayViewSize = min(cellWidth, cellHeight) - cellPadding * 2
-        horizontalPadding = (cellWidth - dayViewSize) / 2
-        verticalPadding = (cellHeight - dayViewSize) / 2
+        dayRect.onViewSizeChanged(width, height)
     }
 
     override fun onDraw(canvas: Canvas) {
-        resetRect()
+        dayRect.reset()
         for (day in days) {
             if (day.currentMonth && day.eventType != EventType.Nothing) drawEvent(canvas, day)
             if (day.isToday) drawToday(canvas)
             if (day.isSelected) drawSelection(canvas)
             drawNumber(canvas, day)
-
-            moveDayRect()
+            dayRect.moveToNext()
         }
-    }
-
-    private fun resetRect() {
-        dayRect.set(
-            horizontalPadding,
-            verticalPadding,
-            horizontalPadding + dayViewSize,
-            verticalPadding + dayViewSize
-        )
     }
 
     private fun drawEvent(canvas: Canvas, day: DayOfMonthUi) {
@@ -78,22 +55,14 @@ class CalendarView @JvmOverloads constructor(
 
     private fun drawDoubleEvent(canvas: Canvas) {
         val paint1 = paints.getForEvent(EventType.MostImportant)
-        canvas.drawArc(dayRect, 90F, 180F, false, paint1)
         val paint2 = paints.getForEvent(EventType.Important)
+        canvas.drawArc(dayRect, 90F, 180F, false, paint1)
         canvas.drawArc(dayRect, -90F, 180F, false, paint2)
     }
 
     private fun drawCircleEvent(day: DayOfMonthUi, canvas: Canvas) {
         val paint = paints.getForEvent(day.eventType)
         canvas.drawOval(dayRect, paint)
-    }
-
-    private fun moveDayRect() {
-        if (dayRect.right + cellWidth <= width) {
-            dayRect.offset(cellWidth, 0f)
-        } else {
-            dayRect.offsetTo(horizontalPadding, dayRect.top + cellHeight)
-        }
     }
 
     private fun drawToday(canvas: Canvas) = canvas.drawOval(dayRect, paints.todayPaint)
@@ -108,7 +77,7 @@ class CalendarView @JvmOverloads constructor(
     }
 
     companion object {
-        private const val COLUMNS = 7
-        private const val ROWS = 6
+        const val COLUMNS = 7
+        const val ROWS = 6
     }
 }
---
+++ main/src/main/java/com/bhaktaprogram/main/custom/view/DayRect.kt
@@ -0,0 +1,39 @@
+package com.bhaktaprogram.main.custom.view
+
+import android.graphics.RectF
+import kotlin.math.min
+
+class DayRect(private val padding: Float) : RectF() {
+    private var cellWidth = 0f
+    private var cellHeight = 0f
+    private var horizontalPadding = 0f
+    private var verticalPadding = 0f
+    private var dayViewSize = 0f
+    private var viewWidth = 0
+
+    fun onViewSizeChanged(width: Int, height: Int) {
+        viewWidth = width
+        cellWidth = width.toFloat() / CalendarView.COLUMNS
+        cellHeight = height.toFloat() / CalendarView.ROWS
+        dayViewSize = min(cellWidth, cellHeight) - padding * 2
+        horizontalPadding = (cellWidth - dayViewSize) / 2
+        verticalPadding = (cellHeight - dayViewSize) / 2
+    }
+
+    fun reset() {
+        set(
+            horizontalPadding,
+            verticalPadding,
+            horizontalPadding + dayViewSize,
+            verticalPadding + dayViewSize
+        )
+    }
+
+    fun moveToNext() {
+        if (right + cellWidth <= viewWidth) {
+            offset(cellWidth, 0f)
+        } else {
+            offsetTo(horizontalPadding, top + cellHeight)
+        }
+    }
+}
