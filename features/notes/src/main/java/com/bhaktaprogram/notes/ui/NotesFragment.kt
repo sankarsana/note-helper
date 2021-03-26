decc065e03ff9df85ef087ef61c04bb545af4853
Sankarsana <sankarsana.nrs@gmail.com>
2021-03-26 16:53:01 +0300

    Add view binding property delegate lib

--- features/notes/src/main/java/com/bhaktaprogram/notes/ui/NotesFragment.kt
+++ features/notes/src/main/java/com/bhaktaprogram/notes/ui/NotesFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bhaktaprogram.coreapi.extensions.getProvidersFacade
 import com.bhaktaprogram.notes.R
 import com.bhaktaprogram.notes.databinding.NotesFragmentBinding
@@ -17,8 +18,7 @@ class NotesFragment : Fragment(R.layout.notes_fragment) {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
     private lateinit var viewModel: NotesViewModel
-    private var _binding: NotesFragmentBinding? = null
-    private val binding get() = _binding!!
+    private val binding by viewBinding(NotesFragmentBinding::bind)
     private val adapter = NotesAdapter()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -28,7 +28,6 @@ class NotesFragment : Fragment(R.layout.notes_fragment) {
             .create(requireActivity().getProvidersFacade())
             .inject(this)
 
-        _binding = NotesFragmentBinding.bind(view)
         binding.list.adapter = adapter
 
         viewModel = ViewModelProvider(this, viewModelFactory)
@@ -41,10 +40,5 @@ class NotesFragment : Fragment(R.layout.notes_fragment) {
 
     private fun updateState(items: List<NoteInfoUiDto>) {
         adapter.update(items)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
