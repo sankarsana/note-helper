f4283cbf2ae9fcc58ac9bfe66a21a88554dc9614
Sankarsana <sankarsana.nrs@gmail.com>
2021-04-30 16:10:37 +0300

    Test main activity in app module

--- app/src/test/java/com/bhaktaprogram/notehelper/MainActivityTest.kt
+++ app/src/test/java/com/bhaktaprogram/notehelper/MainActivityTest.kt
@@ -1,6 +1,7 @@
-package com.bhaktaprogram.main
+package com.bhaktaprogram.notehelper
 
 import android.os.Build
+import com.bhaktaprogram.main.MainActivity
 import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -9,19 +10,17 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-//@Config(manifest = "src/main/AndroidManifest.xml", sdk = [Build.VERSION_CODES.O])
 @Config(sdk = [Build.VERSION_CODES.O])
 class MainActivityTest {
 
     @Test
     fun `should open notes fragment at start`() {
-        val activity = Robolectric.buildActivity(MainActivity::class.java).create().get()
-//        val notesFragment = activity
-//            .supportFragmentManager
-//            .findFragmentById(R.id.fragments_container)
+        val activity = Robolectric.buildActivity(MainActivity::class.java).setup().get()
+        val notesFragment = activity
+            .supportFragmentManager
+            .findFragmentById(R.id.fragments_container)
 
-//        assertNotNull(notesFragment)
-        assertNotNull(1)
+        assertNotNull(notesFragment)
     }
 
 }
